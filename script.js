a044874238bf1a446cf7b27ed77ea7a2e64c0b67
hungle203 <leviethungkm2003@gmail.com>
2021-11-18 14:49:47 +0700

    Ham

---
+++ script.js
@@ -0,0 +1,8 @@
+function square(number) {
+    let result = number * number;
+    return result;
+}
+let squareof2 = square(2);
+document.write(" square of 2 is: " + squareof2 + ",");
+let squareof5 = square(5);
+document.write("Square of 5 is: " +squareof5);
